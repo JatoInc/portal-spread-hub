cdc0ecc57c91e2878f6b146a3ec6cde5489ac1d7
Christian <christianmsbrito@gmail.com>
2019-09-19 22:06:30 -0300

    initial routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloWorldComponent } from '../app/hello-world/hello-world.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'hello-world', component: HelloWorldComponent,
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
+  // exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
