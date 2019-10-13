55dc0163d7eb92f878d6e042df63f59349f4d5a6
Tawan Reis <tawane.reis@linkapi.com.br>
2019-10-13 14:17:39 -0300

    create entities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { CuratorComponent } from './views/curator/curator.component';
+import { StudentComponent } from './views/student/student.component';
+import { ProfessorComponent } from './views/professor/professor.component';
+import { LoginComponent } from './views/login/login.component'; 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    CuratorComponent,
+    StudentComponent,
+    ProfessorComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
