4b1aa114865ffa7747885c5fc4336252eb830985
viniciussreis <viniciusreissantana@gmail.com>
2019-10-15 15:30:16 -0300

    Adding route to curator

--- src/app/infra/app-routing.module.ts
+++ src/app/infra/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HelloWorldComponent } from '../views/hello-world/hello-world.component';
+import { CuratorComponent } from '../views/curator/curator.component';
 
 
 const routes: Routes = [
-  {
-    path: 'hello-world', component: HelloWorldComponent,
-}
+  { path: 'hello-world', component: HelloWorldComponent},
+  { path: 'curator', component: CuratorComponent}
 ];
 
 @NgModule({
--- src/app/infra/app.module.ts
+++ src/app/infra/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from '../views/hello-world/hello-world.component';
 import { RouterModule } from '@angular/router';
+import { CuratorComponent } from '../views/curator/curator.component';
 import { HttpClientModule } from '@angular/common/http'; 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    CuratorComponent
   ],
   imports: [
     BrowserModule,
