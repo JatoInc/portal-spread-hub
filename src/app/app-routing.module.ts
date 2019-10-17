3c8f46f89cdcdfccdf6081fa45189c2a3eeb41fe
Tawan Reis <tawan.reis@linkapi.com.br>
2019-10-17 19:50:01 -0300

    fix files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HelloWorldComponent } from '../app/hello-world/hello-world.component';
+import { LoginComponent } from '../app/views/login/login.component';
 
 
 const routes: Routes = [
   {
     path: 'hello-world', component: HelloWorldComponent,
-}
+  },
+  {
+    path: 'login', component: LoginComponent,
+  }
 ];
 
 @NgModule({
