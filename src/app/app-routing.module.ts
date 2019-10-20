a7dfb5ac850c7c9b95fad601d43515133cc3221e
Tawan Reis <tawan.reis@linkapi.com.br>
2019-10-20 17:44:53 -0300

    fix vinicius's route (professors)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../app/views/login/login.component';
 import { ProfessorListComponent } from './views/professor/professor-list/professor-list.component';
+import { ProfessorDetailsComponent} from './views/professor/professor-details/professor-details.component';
 import { StudentListComponent } from './views/student/student-list/student-list.component';
 import { CuratorListComponent } from './views/curator/curator-list/curator-list.component';
 import { CuratorDetailsComponent } from './views/curator/curator-details/curator-details.component';
-import { ProfessorDetailsComponent} from './views/professor/professor-details/professor-details.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+
   { path: 'professors', component: ProfessorListComponent },
+  { path: 'professors/:id', component: ProfessorDetailsComponent },
+
   { path: 'curators', component: CuratorListComponent },
   { path: 'curators/:id', component: CuratorDetailsComponent },
+
   { path: 'students', component: StudentListComponent },
-  { path: 'vinicius', component: ProfessorDetailsComponent },
 ];
 
 @NgModule({
