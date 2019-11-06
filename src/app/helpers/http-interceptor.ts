6b867502f17eeeb3f261202136a877964aaa31d3
Christian Moura <christianmsbrito@gmail.com>
2019-11-06 20:07:21 -0300

    change interceptor

--- src/app/helpers/http-interceptor.ts
+++ src/app/helpers/http-interceptor.ts
@@ -4,10 +4,13 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.endsWith('/login')) {
+      return next.handle(req);
+    }
     const token = `Basic ${localStorage.getItem('auth_token')}`;
-    const modifiedReq = req.clone({ 
+    const modifiedReq = req.clone({
       headers: req.headers.set('Authorization', token),
     });
     return next.handle(modifiedReq);
